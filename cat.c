680c1e5d52ba9fd35c5f83c35e8a6704fa5eb654
jamestn529 <jamestn529@gmail.com>
2014-12-21 01:05:10 -0700

    Changed output_file() to read many bytes at a time
    Instead of using getc() and putc(), reading one byte at a time (which is very slow), output_file() now reads many bytes at one time into a buffer and writes the buffer to the output file.

--- cat.c
+++ cat.c
@@ -25,10 +25,15 @@ int main(int argc, char **argv) {
 
 }
 
+#define BUFFER_SZ 1024
+
 void output_file(FILE *in, FILE *out) {
-  int chr;
-  while ( (chr = getc(in) ) != EOF ) {
-    putc(chr, out);
+  static const size_t BUFFER_SZ = 1024;
+  static char buffer[BUFFER_SZ];
+  
+  size_t sz; // Number of bytes read by fread()
+  while ((sz = fread(buffer, 1, BUFFER_SZ, in)) != 0) {
+    fwrite(buffer, 1, BUFFER_SZ, out);
   }
 }
 
