46bfa8d008c000fe6d0a1c78d3d6e7143700f942
joshhartigan <joshhartigan99@gmail.com>
2016-05-04 22:22:13 +0100

    fix cat.c line 27

--- cat.c
+++ cat.c
@@ -34,7 +34,7 @@ void output_file(FILE *in, FILE *out) {
   static char buffer[MAX_STRING_SIZE];
 
   size_t size; // Number of bytes read by fread()
-  while ( (size = fread(buffer, 1, MAX_SIZE, in) ) != 0) {
+  while ( (size = fread(buffer, 1, MAX_STRING_SIZE, in) ) != 0) {
     fwrite(buffer, 1, size, out);
   }
 }
